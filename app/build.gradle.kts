c2dc27251d58fcde5c3faf77f5e203e54e36a222
Antaoanji <antaoanji@gmail.com>
2025-12-19 14:16:09 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.pushuptracker"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.pushuptracker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
